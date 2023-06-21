e0bf2f7a900605e60a94e2b3d45c6ec6fc430799
Hassaan Ahmed <hassaankh8@gmail.com>
2023-06-21 11:10:49 -0700

    2.0

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+require("dotenv").config()
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use("/public", express.static('public')); 
+
+const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth: {
+        user: process.env.USER,
+        pass: process.env.PASS
+    }
+})
+app.get("/hello.html", function (req, res) {
+    res.sendFile(__dirname+"/hello.html"); 
+    
+});
+app.get("/", function (req, res) {
+    res.sendFile(__dirname+"/index.html"); 
+    
+});
+app.get("/work.html", function (req, res) {
+    res.sendFile(__dirname+"/work.html"); 
+    
+});
+
+app.post("/hello.html", function (req, res) {
+    nm = req.body.name;
+    email = req.body.email;
+    msg = req.body.msg;
+
+    const mailOptions = {
+    from: email,
+    to: 'hassaankh8@gmail.com',
+    subject: 'Portfolio Email',
+    text: nm +"\n"+ email +"\n"+ msg
+    };
+
+    transporter.sendMail(mailOptions, function(error,info){
+        if(error){
+            console.error();
+        }
+        else{
+            console.log("Email Sent: " + info.response);
+        }
+    })
+
+    return res.redirect('/');
+});
+
+
+app.listen(process.env.PORT || 5500, function () {
+    console.log("localhost:5500");
+});
